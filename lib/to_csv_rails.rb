2d18f4f894c4fd23f4640aa49fc0fdf8def25012
Fernando Morgenstern <contato@fernandomarcelo.com>
2012-12-29 23:26:13 -0200

    Adding default csv renderer to controllers.

--- lib/to_csv_rails.rb
+++ lib/to_csv_rails.rb
@@ -15,3 +15,8 @@ end
 
 require "to_csv_rails/version"
 require "to_csv_rails/array.rb"
+
+ActionController::Renderers.add :csv do |csv, options|
+  self.content_type ||= Mime::CSV
+  self.response_body = csv.respond_to?(:to_csv) ? csv.to_csv : csv
+end
