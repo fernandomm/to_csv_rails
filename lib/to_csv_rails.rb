2f6ffe45ea8c38ba58a240e1a65cc643b2abe7fe
Fernando Morgenstern <contato@fernandomarcelo.com>
2012-12-29 23:28:14 -0200

    Only add renderer if we can find ActionController::Renderers.

--- lib/to_csv_rails.rb
+++ lib/to_csv_rails.rb
@@ -16,7 +16,9 @@ end
 require "to_csv_rails/version"
 require "to_csv_rails/array.rb"
 
-ActionController::Renderers.add :csv do |csv, options|
-  self.content_type ||= Mime::CSV
-  self.response_body = csv.respond_to?(:to_csv) ? csv.to_csv : csv
+if defined?(ActionController::Renderers) && ActionController::Renderers.respond_to?(:add)
+  ActionController::Renderers.add :csv do |csv, options|
+    self.content_type ||= Mime::CSV
+    self.response_body = csv.respond_to?(:to_csv) ? csv.to_csv : csv
+  end
 end
